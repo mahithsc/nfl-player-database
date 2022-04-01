1e570d723428311de2dc46107a2e77119b81932f
mahithsc <mahith.chitrapu@gmail.com>
2022-04-01 09:55:19 -0400

    getting all the patriots plays

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const fs = require('fs/promises');
 
 (async () => {
-    const browser = await puppeteer.launch({});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://www.nfl.com/teams/new-england-patriots/roster');
+    await page.screenshot({path: 'roster.png', fullPage: true});
+
+    const players = await page.$$eval('.nfl-o-roster__player-name.nfl-o-cta--link', players => {
+        return players.map(player => player.textContent)
+    })
+    await fs.writeFile('names.txt', players.join('\r\n'))
+
+    await browser.close();
 })();
