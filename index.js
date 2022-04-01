75a2dd64a82d5fa7ab6bd76b784bd6af6a6f6e85
mahithsc <mahith.chitrapu@gmail.com>
2022-04-01 17:56:02 -0400

    removing uneeded code

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ const teamsID = [
     'https://www.espn.com/nfl/team/depth/_/name/ten',
     'https://www.espn.com/nfl/team/depth/_/name/den',
     'https://www.espn.com/nfl/team/depth/_/name/kc',
-    'https://www.espn.com/nfl/team/depth/_/name/oak',
-    'https://www.espn.com/nfl/team/depth/_/name/sd',
+    'https://www.espn.com/nfl/team/depth/_/name/lac/los-angeles-chargers',
     'https://www.espn.com/nfl/team/depth/_/name/dal',
     'https://www.espn.com/nfl/team/depth/_/name/nyg',
     'https://www.espn.com/nfl/team/depth/_/name/phi',
-    'https://www.espn.com/nfl/team/depth/_/name/was',
+    'https://www.espn.com/nfl/team/depth/_/name/wsh/washington',
     'https://www.espn.com/nfl/team/depth/_/name/chi',
     'https://www.espn.com/nfl/team/depth/_/name/det',
     'https://www.espn.com/nfl/team/depth/_/name/gb',
@@ -43,27 +42,26 @@ const teamsID = [
     const page = await browser.newPage();
 
     for (const id of teamsID) {
+        const teamname = id.slice(42)
         await page.goto(id);
+        const qb = await page.$eval('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(1) > td:nth-child(1) > span > a', el => el.innerText);
+        const rb = await page.$eval('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(2) > td:nth-child(1) > span > a', el => el.innerText);
+        const wr = await page.$eval('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(3) > td:nth-child(1) > span > a', el => el.innerText);
+        const wrTwo = await page.$eval('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(4) > td:nth-child(1) > span > a', el => el.innerText);
+        const wrThree = await page.$eval('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(5) > td:nth-child(1) > span > a', el => el.innerText);
+        const te = await page.$eval('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(6) > td:nth-child(1) > span > a', el => el.innerText);
 
-        const quarterback = await page.evaluate(() => {
-            const qb = document.querySelectorAll('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(1) > td:nth-child(1) > span > a');
-            const qbName = qb.innerText;
-        })
+        const team = {
+            qb: qb,
+            rb: rb,
+            wr: wr,
+            wrTwo: wrTwo,
+            wrThree: wrThree,
+            te: te
+        }
+
+        fs.appendFile('players.json', JSON.stringify(team,))
+
 
     }
 })();
-
-
-// (async () => {
-//     const browser = await puppeteer.launch({headless: false});
-//     const page = await browser.newPage();
-//     await page.goto('https://www.espn.com/nfl/team/depth/_/name/buf/buffalo-bills');
-//     await page.screenshot({path: 'roster.png', fullPage: true});
-
-//     const players = await page.$$eval('#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(1) > td:nth-child(1) > span > a', players => {
-//         return players.map(player => player.textContent)
-//     })
-//     await fs.writeFile('players.json', players.join('\r\n'))
-
-//     await browser.close();
-// })();
